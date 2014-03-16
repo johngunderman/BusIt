4a38537015b530477539240d196520e311d8fa7b
Tom Dooner <tomdooner@gmail.com>
2014-03-16 14:04:33 -0700

    Add clickability to the map

--- src/com/example/busit/MainActivity.java
+++ src/com/example/busit/MainActivity.java
@@ -14,6 +14,7 @@ import org.json.JSONTokener;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -26,8 +27,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
 
@@ -36,6 +37,7 @@ public class MainActivity extends Activity {
     private TextView textView;
     private MapView mapView;
     private GoogleMap map;
+    private JSONArray busLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +50,56 @@ public class MainActivity extends Activity {
         this.map = this.mapView.getMap();
         MapsInitializer.initialize(getApplicationContext());
 
-        this.map.moveCamera(CameraUpdateFactory.newLatLng(
-                new LatLng(37.7745952,-122.456628)));
+        this.map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(
+                37.7745952, -122.456628)));
 
         this.map.moveCamera(CameraUpdateFactory.zoomTo(18.0f));
 
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
+        this.map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            public JSONObject getClosestBus(LatLng location) {
+                double distance = Float.MAX_VALUE;
+                JSONObject closestBus = null;
+
+                try {
+                    for (int i = 0; i < busLocations.length(); i++) {
+                        JSONObject bus = (JSONObject) busLocations.get(i);
+                        double lat = bus.getDouble("lat");
+                        double lon = bus.getDouble("lon");
+
+                        double thisDistance = Math.sqrt(Math.pow(
+                                (location.latitude - lat), 2)
+                                + Math.pow((location.longitude - lon), 2));
+
+                        if (thisDistance < distance) {
+                            distance = thisDistance;
+                            closestBus = bus;
+                        }
+                    }
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+
+                return closestBus;
+            }
+
+            @Override
+            public void onMapClick(LatLng arg0) {
+                JSONObject closestBus = getClosestBus(arg0);
+
+                try {
+                    if (closestBus != null) {
+                        textView.setText((String) closestBus.get("route"));
+                    } else {
+                        throw new JSONException("No Closest Bus D:");
+                    }
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
             new RenderBusListTask().execute();
@@ -136,14 +181,18 @@ public class MainActivity extends Activity {
         private void renderBusData(JSONObject busData) {
             textView.setText(busData.toString());
 
-            JSONArray busArray;
             try {
-                busArray = busData.getJSONArray("results");
-                for (int i = 0; i < busArray.length(); i++) {
-                    JSONObject busLocation = (JSONObject) busArray.get(i);
+                busLocations = busData.getJSONArray("results");
+                for (int i = 0; i < busLocations.length(); i++) {
+                    JSONObject busLocation = (JSONObject) busLocations.get(i);
                     double lat = busLocation.getDouble("lat");
                     double lon = busLocation.getDouble("lon");
-                    map.addMarker((new MarkerOptions()).position(new LatLng(lat, lon)));
+                    LatLng location = new LatLng(lat, lon);
+
+                    // map.addMarker((new MarkerOptions()).position());
+                    map.addCircle((new CircleOptions()).radius(10.0)
+                            .fillColor(Color.RED).center(location)
+                            .strokeWidth(3f));
                 }
             } catch (JSONException e) {
                 // TODO Auto-generated catch block
