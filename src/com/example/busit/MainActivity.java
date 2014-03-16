a93c9b58a0d88decc1273362e6be548db7f5205a
Tom Dooner <tomdooner@gmail.com>
2014-03-16 13:11:44 -0700

    Add working map
    Also add bin to .gitignore

--- src/com/example/busit/MainActivity.java
+++ src/com/example/busit/MainActivity.java
@@ -21,14 +21,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends Activity {
 
-	private static final String ALL_BUS_DATA_URL = "http://busit.herokuapp.com/buses";
-	private static final String DEBUG_TAG = "MainActivity";
-	private TextView textView;
-	private MapView mapView;
+    private static final String ALL_BUS_DATA_URL = "http://busit.herokuapp.com/buses";
+    private static final String DEBUG_TAG = "MainActivity";
+    private TextView textView;
+    private MapView mapView;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,84 +43,123 @@ public class MainActivity extends Activity {
         this.mapView = (MapView) findViewById(R.id.map_view);
         this.mapView.onCreate(savedInstanceState);
 
+        this.map = this.mapView.getMap();
+        MapsInitializer.initialize(getApplicationContext());
+
+        this.map.moveCamera(CameraUpdateFactory.newLatLng(
+                new LatLng(37.7745952,-122.456628)));
+
+        this.map.moveCamera(CameraUpdateFactory.zoomTo(18.0f));
+
         ConnectivityManager connMgr = (ConnectivityManager)
-        		getSystemService(Context.CONNECTIVITY_SERVICE);
+                getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-        	new RenderBusListTask().execute();
+            new RenderBusListTask().execute();
         } else {
-        	Log.d(DEBUG_TAG, "Couldn't connect to the network!");
+            Log.d(DEBUG_TAG, "Couldn't connect to the network!");
         }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.mapView.onDestroy();
+        this.mapView = null;
+    }
+
+    @Override
+    public final void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        this.mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public final void onLowMemory() {
+        super.onLowMemory();
+        this.mapView.onLowMemory();
     }
 
     private class RenderBusListTask extends AsyncTask<Void, Void, JSONObject> {
 
-    	private void renderNearbyBusData() {
-    		JSONObject busData;
-    		try {
-    			busData = getNearbyBusData();
-    			renderBusData(busData);
-    		} catch (IOException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		} catch (JSONException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}
-    	}
+        private void renderNearbyBusData() {
+            JSONObject busData;
+            try {
+                busData = getNearbyBusData();
+                renderBusData(busData);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
 
-    	private JSONObject getNearbyBusData() throws IOException, JSONException {
-    		URL url = new URL(ALL_BUS_DATA_URL);
-    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    		conn.setReadTimeout(10000 /* milliseconds */);
-    		conn.setConnectTimeout(15000 /* milliseconds */);
-    		conn.setRequestMethod("GET");
-    		conn.setDoInput(true);
-    		// Starts the query
-    		conn.connect();
-    		int response = conn.getResponseCode();
-    		Log.d(DEBUG_TAG, "The response is: " + response);
-    		InputStream in = new BufferedInputStream(conn.getInputStream());
-    		Scanner scanner = new Scanner(in);
-    		String input = "";
-    		try {
-    			input = scanner.next();
-    		} finally {
-    			in.close();
-    			scanner.close();
-    		}
-    		return new JSONObject(new JSONTokener(input));
-    	}
+        private JSONObject getNearbyBusData() throws IOException, JSONException {
+            URL url = new URL(ALL_BUS_DATA_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            Scanner scanner = new Scanner(in);
+            String input = "";
+            try {
+                input = scanner.next();
+            } finally {
+                in.close();
+                scanner.close();
+            }
+            return new JSONObject(new JSONTokener(input));
+        }
 
-    	private void renderBusData(JSONObject busData) {
-    		textView.setText(busData.toString());
-    	}
+        private void renderBusData(JSONObject busData) {
+            textView.setText(busData.toString());
+        }
 
-    	@Override
-    	protected JSONObject doInBackground(Void... params) {
-    		try {
-    			return getNearbyBusData();
-    		} catch (IOException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		} catch (JSONException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}
-    		return null;
-    	}
+        @Override
+        protected JSONObject doInBackground(Void... params) {
+            try {
+                return getNearbyBusData();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-    	@Override
-    	protected void onPostExecute(JSONObject result) {
-    		renderBusData(result);
-    	}
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            renderBusData(result);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	// Inflate the menu; this adds items to the action bar if it is present.
-    	getMenuInflater().inflate(R.menu.main, menu);
-    	return true;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
 }
