34b523422e62ebb31a2a675ddd8a48af6f57d0af
John Gunderman <johngunderman@gmail.com>
2014-03-15 22:04:23 -0700

    Fix app not starting due to http request in main thread

--- src/com/example/busit/MainActivity.java
+++ src/com/example/busit/MainActivity.java
@@ -3,75 +3,106 @@ package com.example.busit;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
 import java.util.Scanner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
-import android.util.JsonReader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private static final String ALL_BUS_DATA_URL = "http://busit.herokuapp.com/busses";
-	
+	private static final String ALL_BUS_DATA_URL = "http://busit.herokuapp.com/buses";
+	private static final String DEBUG_TAG = "MainActivity";
+	private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ConnectivityManager connMgr = (ConnectivityManager) 
+        this.textView = (TextView) findViewById(R.id.default_text);
+        ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()) {
-                renderNearbyBusData();
+                new RenderBusListTask().execute();
             } else {
-                // display error
+                Log.d(DEBUG_TAG, "Couldn't connect to the network!");
             }
     }
-    
-    private void renderNearbyBusData() {
-    	JSONObject busData;
-		try {
-			busData = getNearbyBusData();
-	    	renderBusData(busData);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-    
-    private JSONObject getNearbyBusData() throws IOException, JSONException {
-    	URL url = new URL(ALL_BUS_DATA_URL);
-    	URLConnection urlConnection = url.openConnection();
-		InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-		Scanner scanner = new Scanner(in);
-		String input = "";
-    	try {
-    		input = scanner.next();
-    	} finally {
-    		in.close();
-    		scanner.close();
-    	}
-    	return new JSONObject(new JSONTokener(input));
-    }
 
-    private void renderBusData(JSONObject busData) {
-    	TextView view = new TextView(this);
-    	view.setText(busData.toString().toCharArray(), 0, busData.toString().length());
+    private class RenderBusListTask extends AsyncTask<Void, Void, JSONObject> {
+
+    	private void renderNearbyBusData() {
+    		JSONObject busData;
+    		try {
+    			busData = getNearbyBusData();
+    			renderBusData(busData);
+    		} catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (JSONException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    	}
+
+    	private JSONObject getNearbyBusData() throws IOException, JSONException {
+    		URL url = new URL(ALL_BUS_DATA_URL);
+    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    		conn.setReadTimeout(10000 /* milliseconds */);
+    		conn.setConnectTimeout(15000 /* milliseconds */);
+    		conn.setRequestMethod("GET");
+    		conn.setDoInput(true);
+    		// Starts the query
+    		conn.connect();
+    		int response = conn.getResponseCode();
+    		Log.d(DEBUG_TAG, "The response is: " + response);
+    		InputStream in = new BufferedInputStream(conn.getInputStream());
+    		Scanner scanner = new Scanner(in);
+    		String input = "";
+    		try {
+    			input = scanner.next();
+    		} finally {
+    			in.close();
+    			scanner.close();
+    		}
+    		return new JSONObject(new JSONTokener(input));
+    	}
+
+    	private void renderBusData(JSONObject busData) {
+    		textView.setText(busData.toString());
+    	}
+
+		@Override
+		protected JSONObject doInBackground(Void... params) {
+			try {
+				return getNearbyBusData();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject result) {
+			renderBusData(result);
+		}
     }
 
     @Override
@@ -80,5 +111,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
 }
